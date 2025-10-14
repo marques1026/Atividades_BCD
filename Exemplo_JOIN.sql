5bc1097d69cd4b2a4c559979f9701d35ee0e995e
Giovanna Marques <giovannamarquesz.lima@gmail.com>
2025-10-14 15:57:16 -0300

    Add files via upload

---
+++ Exemplo_JOIN.sql
@@ -0,0 +1,21 @@
+USE Biblioteca_Gi;
+
+-- SELECT
+	-- Livro.titulo,
+    -- Autor.nome AS nome_autor,
+    -- categoria.nome AS CATEGORIA,
+    -- usuario.nome,
+    -- emprestimo.data_emprestimo,
+	-- emprestimo.data_limite_devolucao
+
+-- FROM Livro
+-- INNER JOIN Autor_livro ON Livro.id_livro = Autor_Livro.id_livro
+-- INNER JOIN Autor ON Autor.id_autor = Autor_livro.id_autor
+-- INNER JOIN categoria_livro ON Livro.id_livro = categoria_livro.id_livro 
+-- INNER JOIN categoria ON categoria_livro.id_categoria = categoria.id_categoria
+-- INNER JOIN emprestimo ON emprestimo.id_livro = Livro.id_livro
+-- INNER JOIN usuario ON usuario.id_usuario = emprestimo.id_usuario
+
+
+
+
